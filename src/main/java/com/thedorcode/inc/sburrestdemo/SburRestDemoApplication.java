d521399d8299d7657070fb8765d95dbba8c480ea
tshimsthedoor <tshims79@gmail.com>
2023-11-08 11:23:58 +0200

    we played around with the properties file

--- src/main/java/com/thedorcode/inc/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thedorcode/inc/sburrestdemo/SburRestDemoApplication.java
@@ -1,8 +1,11 @@
 package com.thedorcode.inc.sburrestdemo;
 
+import com.thedorcode.inc.sburrestdemo.shop.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,6 +13,12 @@ public class SburRestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix="droid")
+	Droid createDroid() {
+		return new Droid();
 	}
 
 }
---
+++ src/main/java/com/thedorcode/inc/sburrestdemo/shop/Droid.java
@@ -0,0 +1,17 @@
+package com.thedorcode.inc.sburrestdemo.shop;
+
+ public class Droid {
+    private String id, description;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
